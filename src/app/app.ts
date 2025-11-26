b55d368286aa991713a28991165ad024ef7166fd
Prasadgurav07 <prasadgurav2612@gmail.com>
2025-11-26 19:05:23 +0530

    2

--- src/app/app.ts
+++ src/app/app.ts
@@ -2,12 +2,16 @@ import { Component } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
 import { Home } from "./mycomponents/home/home";
 
+
 @Component({
   selector: 'app-root',
   imports: [RouterOutlet, Home],   // ⬅ Add both components here
   templateUrl: './app.html',
   styleUrl: './app.css'
 })
+
 export class App {
-  title = 'Developer';
+  
+
+
 }
--- src/app/mycomponents/home/home.ts
+++ src/app/mycomponents/home/home.ts
@@ -1,6 +1,8 @@
 import { Component, signal } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { CUSTOM_ELEMENTS_SCHEMA } from '@angular/core';   
+import { HttpClient } from '@angular/common/http';
+import { ApiService } from '../../services/user';
 
 @Component({
   selector: 'app-home',
@@ -11,9 +13,20 @@ import { CUSTOM_ELEMENTS_SCHEMA } from '@angular/core';
   schemas: [CUSTOM_ELEMENTS_SCHEMA]
 })
 export class Home {
-  
+  fullname='data';
+  employee: any = null;   // store 1 employee
 
+  constructor(private api: ApiService) {}
+  ngOnInit() {
+    this.loadEmployee();
+  }
 
+  loadEmployee() {
 
+    this.api.getEmployee(1).subscribe(res => {
+      this.employee = res[0];
+      console.log(this.employee);
+    });
+  }
 
 }
